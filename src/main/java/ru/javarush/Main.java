a756407b0421a867ae0c4a4d9d2cc4cd27477f85
Alimov Dmitriy <al.dmitriy@ya.ru>
2026-06-07 18:59:07 +0300

    Merge remote-tracking branch 'origin/Dmitriev-Kirill into Alimov-Dmitry

--- src/main/java/ru/javarush/Main.java
+++ src/main/java/ru/javarush/Main.java
@@ -1,17 +1,8 @@
 package ru.javarush;
 
-import ru.javarush.model.Car;
-
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
 
-        Car.CarBuilder carBuilder = new Car.CarBuilder();
-
-        Car car = carBuilder.setModel("").setPower(0).setReleaseYear(LocalDate.now()).build();
-
-        System.out.println(car);
 
     }
 }
