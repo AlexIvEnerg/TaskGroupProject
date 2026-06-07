ff76281555fe721d98e59eb910e3b65582977b2b
Kirill <kirill.dmitriev.666@mail.com>
2026-06-07 13:34:10 +0300

    add sort and comparator packages

---
+++ src/main/java/ru/javarush/sort/EvenOddSort.java
@@ -0,0 +1,28 @@
+package ru.javarush.sort;
+
+import ru.javarush.model.Car;
+import java.util.Comparator;
+
+public class EvenOddSort {
+    public void evenOddSort(Car[] cars, Comparator<Car> comparator) {
+        for (int i = 0; i < cars.length - 1; i++) {
+            if (cars[i].getPower() % 2 != 0) {
+                continue;
+            } else {
+                int minIndex = i;
+                for (int j = i + 1; j < cars.length; j++) {
+                    if (cars[j].getPower() % 2 == 0) {
+                        if (comparator.compare(cars[j], cars[minIndex]) < 0) {
+                            minIndex = j;
+                        }
+                    }
+                }
+                if (minIndex != i) {
+                    Car temp = cars[i];
+                    cars[i] = cars[minIndex];
+                    cars[minIndex] = temp;
+                }
+            }
+        }
+    }
+}
