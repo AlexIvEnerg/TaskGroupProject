2c9274c1534abaf25da8aad130791b7b57c60857
bpi90437 <bpi90437@gmail.com>
2026-06-09 15:12:36 +0600

    Add CarWriter

---
+++ src/main/java/service/CarWriter.java
@@ -0,0 +1,32 @@
+package service;
+
+import model.Car;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Collection;
+
+public class CarWriter {
+    private static final String FIELD_SEPARATOR = ";";
+    public void appendCars(Collection<Car> cars,String fileName){
+        try {
+            StringBuilder builder = new StringBuilder();
+            for (Car car : cars) {
+                builder.append(car.getModel())
+                        .append(FIELD_SEPARATOR)
+                        .append(car.getPower())
+                        .append(FIELD_SEPARATOR)
+                        .append(car.getReleaseDate())
+                        .append(System.lineSeparator());
+
+            }
+            Files.writeString(Path.of(fileName),builder.toString(), StandardOpenOption.CREATE,StandardOpenOption.APPEND);
+
+        }catch (IOException e){
+            throw new RuntimeException("Cannot write file",e);
+        }
+
+    }
+}
